bcc9d9fd02ba9edad1b3a3eb8af392b428031491
binbin666 <iotcoder@gmail.com>
2022-02-02 23:32:55 +0800

    -- [modify] update version

--- src/main/java/site/iotor/hello/spring/config/AppConfig.java
+++ src/main/java/site/iotor/hello/spring/config/AppConfig.java
@@ -6,21 +6,8 @@
  */
 package site.iotor.hello.spring.config;
 
-import static org.hibernate.cfg.AvailableSettings.C3P0_ACQUIRE_INCREMENT;
-import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_SIZE;
-import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_STATEMENTS;
-import static org.hibernate.cfg.AvailableSettings.C3P0_MIN_SIZE;
-import static org.hibernate.cfg.AvailableSettings.C3P0_TIMEOUT;
-import static org.hibernate.cfg.AvailableSettings.DRIVER;
-import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
-import static org.hibernate.cfg.AvailableSettings.PASS;
-import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
-import static org.hibernate.cfg.AvailableSettings.URL;
-import static org.hibernate.cfg.AvailableSettings.USER;
-
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
@@ -34,6 +21,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import site.iotor.hello.spring.model.Authorities;
 import site.iotor.hello.spring.model.PersistentLogins;
 import site.iotor.hello.spring.model.User;
+
+import static org.hibernate.cfg.AvailableSettings.*;
 
 /**
  * @author Rancho
@@ -65,15 +54,16 @@ public class AppConfig {
         props.put(PASS, env.getProperty("mysql.password"));
 
         // Setting Hibernate properties
-        props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
-        props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+        props.put(SHOW_SQL, env.getProperty(SHOW_SQL));
+        props.put(HBM2DDL_AUTO, env.getProperty(HBM2DDL_AUTO));
+        props.put(DIALECT, env.getProperty(DIALECT));
 
         // Setting C3P0 properties
-        props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
-        props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
-        props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
-        props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
-        props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
+        props.put(C3P0_MIN_SIZE, env.getProperty(C3P0_MIN_SIZE));
+        props.put(C3P0_MAX_SIZE, env.getProperty(C3P0_MAX_SIZE));
+        props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty(C3P0_ACQUIRE_INCREMENT));
+        props.put(C3P0_TIMEOUT, env.getProperty(C3P0_TIMEOUT));
+        props.put(C3P0_MAX_STATEMENTS, env.getProperty(C3P0_MAX_STATEMENTS));
 
         factoryBean.setHibernateProperties(props);
         factoryBean.setAnnotatedClasses(User.class, Authorities.class, PersistentLogins.class);
